bfa628e2063d0decb8e2557e29fe5465d2d3e8ec
Gowri Sankhar S <166432636+GowriSankhar04@users.noreply.github.com>
2025-06-25 02:14:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,5 +25,9 @@ def extract():
 
     return jsonify(prediction_result)
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
